4a599425b07240561b0ec98eb97587a644f0c660
MarkGhebrial <mghebrialjr@icloud.com>
2024-04-02 17:34:55 -0700

    Save bot configuration for each guild

--- src/config.rs
+++ src/config.rs
@@ -1,5 +1,15 @@
 use serenity::all::GuildId;
 
+use serde::{Deserialize, Serialize};
+
+use std::fs::{File, DirBuilder};
+use std::path::Path;
+
+use std::io::{Read, Write};
+
+static CONFIG_PATH: &str = "/home/markg/.config/dart-sweeper";
+
+#[derive(Deserialize, Serialize)]
 pub struct BotConfig {
     pub whitelisted_roles: Vec<String>,
     pub mod_log_channel_id: Option<u64>,
@@ -8,21 +18,43 @@ pub struct BotConfig {
 impl Default for BotConfig {
     fn default() -> Self {
         Self {
-            whitelisted_roles: Default::default(),
+            whitelisted_roles: vec!["Verified".into()],
             mod_log_channel_id: Default::default(),
         }
     }
 }
 
 pub fn get_config(guild: GuildId) -> BotConfig {
-    match guild.get() {
-        1042992678469632031 => BotConfig {
-            whitelisted_roles: vec!["Verified".into(), "Trusted".into()],
-            mod_log_channel_id: Some(1224851810200584222),
-        },
-        _ => BotConfig {
-            whitelisted_roles: vec!["Verified".into()],
-            mod_log_channel_id: None,
-        },
-    }
+    let path = Path::new(CONFIG_PATH);
+    let path = path.join(guild.get().to_string());
+
+    println!("Path is {:?}", path);
+
+    let mut file = match File::open(&path) {
+        Ok(file) => file,
+        Err(_e) => {
+            DirBuilder::new().recursive(true).create(CONFIG_PATH).unwrap();
+
+            match File::create(&path) {
+                Ok(mut file) => {
+                    let file_contents = toml::to_string(&BotConfig::default()).unwrap();
+
+                    file.write(file_contents.as_bytes()).unwrap();
+                    file
+                },
+                Err(e) => panic!("{:?}", e)
+            }
+        }
+    };
+
+    // let mut file = OpenOptions::new()
+    //     .read(true)
+    //     .write(true)
+    //     .create(true)
+    //     .open(path)
+    //     .unwrap();
+
+    let mut s = String::new();
+    file.read_to_string(&mut s).unwrap();
+    toml::from_str(&s).unwrap()
 }
