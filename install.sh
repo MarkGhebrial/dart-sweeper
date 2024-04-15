6ea9d19a32ddf1cfe97e6f3880851c3c28799541
MarkGhebrial <mghebrialjr@icloud.com>
2024-04-15 11:36:19 -0700

    Add systemd config and install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+cargo build --release
+
+sudo cp target/release/dart-sweeper /usr/bin/dart-sweeper
+sudo cp dart-sweeper.service /etc/systemd/system/dart-sweeper.service
+
+sudo systemctl enable dart-sweeper
+sudo systemctl start dart-sweeper
